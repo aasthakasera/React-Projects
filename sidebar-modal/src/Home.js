e275cce3974ba3bf52e137615cbdf3adb1c8d5cd
aasthakasera <aasthakasera2000@gmail.com>
2022-01-22 11:08:47 +0530

    Refactor Navbar

--- sidebar-modal/src/Home.js
+++ sidebar-modal/src/Home.js
@@ -5,14 +5,7 @@ import { useGlobalContext } from './context';
 const Home = () => {
   const { openSidebar, openModal } = useGlobalContext();
   return (
-    <main>
-      <button onClick={openSidebar} className='sidebar-toggle'>
-        <FaBars />
-      </button>
-      <button onClick={openModal} className='btn'>
-        show modal
-      </button>
-    </main>
+    <h1></h1>
   );
 };
 
